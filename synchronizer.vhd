1f8b1104e0cabcf37d260b237fc5d758b525949b
Ryan <ryanstefanov04@gmail.com>
2023-12-21 22:58:44 -0500

    Update synchronizer.vhd

--- synchronizer.vhd
+++ synchronizer.vhd
@@ -1,45 +1,34 @@
---Author: Group 10, Ryan Stefanov, Ryan Wang
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
 
+-- Entity declaration for synchronizer
+ENTITY synchronizer IS
+    PORT (
+        clk   : IN  std_logic;  -- Connected to the 50MHz common Global Clock (clkin_50)
+        reset : IN  std_logic;  -- Resets to 0
+        din   : IN  std_logic;  -- Original input that gets passed in
+        dout  : OUT std_logic   -- Final output
+    );
+END synchronizer;
 
-entity synchronizer is port ( -- the inputs are asynchronous so they must be synchronized with the clock. This is to avoid metastability
-
-			clk					: in std_logic; -- Connected to the 50MHz common Global Clock (clkin_50)
-			reset					: in std_logic; -- resets to 0
-			din					: in std_logic; -- original input thta gets passed in
-			dout					: out std_logic -- final output
-  );
- end synchronizer;
- 
- 
-architecture circuit of synchronizer is
-
-	Signal sreg				: std_logic_vector(1 downto 0); --as the first output, Q
+-- Architecture of synchronizer
+ARCHITECTURE circuit OF synchronizer IS
+    SIGNAL sreg : std_logic_vector(1 downto 0); -- First output, Q
 
 BEGIN
+    -- Synchronize process to align inputs with the clock
+    synchronize : PROCESS (clk, reset, din)
+    BEGIN
+        IF rising_edge(clk) THEN
+            IF reset = '1' THEN
+                sreg <= "00"; -- Reset synchronously on a rising clock edge
+            ELSE
+                sreg <= sreg(0) & din; -- Process through the first D flip-flop
+            END IF;
+        END IF;
 
-	synchronize: process (clk, reset, din) is -- This process occurs everytime a rising edge occurs 
-	
-	begin
-	
-		if( rising_edge(clk) ) then
-		
-			if (reset = '1') then -- notice that reset is synchronous, therefore it can only reset on a rising clock edge
-				sreg <= "00";
-			
-			else 
-				sreg <= sreg(0) & din; -- process through the first D flip flop
-				
-			end if;
+        dout <= sreg(1); -- Final output from the second D flip-flop's output
+    END PROCESS synchronize;
 
-		else
-		
-		end if;
-		
-	dout <= sreg(1); -- the final output comes from the second D flip flop's output
-		
-	end process;
-
-END;
+END circuit;
